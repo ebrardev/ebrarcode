05f2b6a2458ce7f8a224c5abff7b3a5e6f143dd2
Ebrar <73947103+ebrardev@users.noreply.github.com>
2024-03-21 15:43:35 +0300

    Initial commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,10 @@
+export const contactsData = {
+    email: 'altunkaynakebrar@gmail.com',
+    phone: '+90 553 589 59 90',
+    address: 'Ankara, Turkey',
+    github: 'https://github.com/ebrardev',
+    linkedIn: 'https://www.linkedin.com/in/ebrarmeadev/',
+    twitter: 'https://twitter.com/ebrardev',
+    stackOverflow: 'https://stackoverflow.com/users/16289546/ebrardev',
+    devUsername: "ebrardev"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Ebrar Altunkaynak",
+  profile: '/profile.png',
+  designation: "Software Enginer",
+  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'altunkaynakebrar@gmail.com',
+  phone: '+90 553 589 59 90',
+  address: 'Ankara, Turkey',
+  github: 'https://github.com/ebrardev',
+  facebook: 'https://www.facebook.com',
+  linkedIn: 'https://www.linkedin.com/in/ebrarmeadev/',
+  twitter: 'https://twitter.com/ebrardev',
+  stackOverflow: 'https://stackoverflow.com/users/16289546/ebrardev',
+  leetcode: "https://leetcode.com/ebrardev/",
+  devUsername: "ebrardev",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
